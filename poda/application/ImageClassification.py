[Update] Fixing batch normalization

poda/application/ImageClassification.py
@@ -72,6 +72,7 @@ class ImageClassification(object):
             else:
                 non_logit , output , base_var_list , full_var_list = vgg16(input_tensor=self.input_tensor,num_classes=self.classes,num_blocks=num_block,num_depthwise_layer=num_depthwise_layer,
                                                                        num_fully_connected_layer=num_dense_layer,num_hidden_unit=num_hidden_unit,activation_fully_connected=activation_dense,regularizers=regularizer)
+        """
         elif self.type_architecture in 'inception_v4_slim':
             if len(dict_architecture) > 0:
                 inception_a = dict_architecture['n_inception_a']
@@ -100,7 +101,7 @@ class ImageClassification(object):
             else:
                 non_logit, output, base_var_list, full_var_list = inception_v4(inputs=self.input_tensor,  num_classes=self.classes,final_endpoint='Mixed_7d',is_training=batch_normalization,dropout_keep_prob=dropout_rate,
                                                                                 num_depthwise_layer=None,regularizers=regularizer)
-
+            """
         return non_logit, output, base_var_list, full_var_list
 
     def train(self, epoch, output_model_path, dict_augmented_image={}, is_last_checkpoint=False, manual_split_dataset= False, use_pretrain = False, path_pretrained='', threshold_best_model=0.5, optimizers_name='adam', lr=0.0001):
@@ -168,6 +169,9 @@ class ImageClassification(object):
         if not os.path.exists(main_graph_saver_path):
             os.makedirs(main_graph_saver_path)
 
+        base_model_path = os.path.join(main_graph_saver_path,"base_"+str(self.type_architecture))
+        full_model_path = os.path.join(output_saver_path,str(self.type_architecture))
+
         msg = "Epoch: {0:>6} loss: {1:>6.3} - acc: {2:>6.3} - val_loss: {3:>6.3} - val_acc: {4:>6.3} - {5}"
         
         train_iteration = int(num_train_data/self.batch_size)
@@ -183,12 +187,12 @@ class ImageClassification(object):
             sess.run(init)
             
             if is_last_checkpoint:
-                loader_output = tf.compat.v1.train.import_meta_graph(output_saver_path + '.meta')
+                loader_output = tf.compat.v1.train.import_meta_graph(full_model_path + '.meta')
                 loader_output.restore(sess, output_saver_path)
                 #output_saver.restore(sess,output_saver_path)
 
             if use_pretrain:
-                loader_main_graph = tf.compat.v1.train.import_meta_graph(main_graph_saver_path + '.meta')
+                loader_main_graph = tf.compat.v1.train.import_meta_graph(base_model_path + '.meta')
                 loader_main_graph.restore(sess, main_graph_saver_path)
                 #main_graph_saver.restore(sess,main_graph_saver_path)            
             
@@ -232,8 +236,6 @@ class ImageClassification(object):
                 avg_val_acc = sum(tmp_val_acc)/(len(tmp_val_acc)+0.0001)
                     
                 if avg_val_acc > best_val_accuracy:
-                    base_model_path = os.path.join(main_graph_saver_path,"base_"+str(self.type_architecture))
-                    full_model_path = os.path.join(output_saver_path,str(self.type_architecture))
                     main_graph_saver.save(sess=sess,save_path=base_model_path)
                     output_saver.save(sess=sess,save_path=full_model_path)
                     best_val_accuracy = avg_val_acc

poda/layers/convolutional.py
@@ -93,7 +93,7 @@ def avarage_pool_3d(input_tensor, kernel_sizes=(3,3,3), stride_sizes=(1,1,1), pa
     layer = tf.nn.avg_pool3d(value=input_tensor,ksize=kernel_sizes,strides=stride_sizes,padding=paddings,data_format='NHWC',name=names)
     return layer
 
-def batch_normalization(input_tensor, is_trainable=True, decay = 0.999, epsilon = 1e-3):
+def batch_normalization(input_tensor, is_trainable=True):
     """[summary]
     
     Arguments:
@@ -105,20 +105,7 @@ def batch_normalization(input_tensor, is_trainable=True, decay = 0.999, epsilon 
     Returns:
         [Tensor] -- [A trainable tensor]
     """
-    scale = tf.Variable(tf.ones([input_tensor.get_shape()[-1]]))
-    beta = tf.Variable(tf.zeros([input_tensor.get_shape()[-1]]))
-    pop_mean = tf.Variable(tf.zeros(input_tensor.get_shape().as_list()[-3:]), trainable=False)
-    pop_var = tf.Variable(tf.ones(input_tensor.get_shape().as_list()[-3:]), trainable=False)
-
-    if is_trainable:
-        batch_mean, batch_var = tf.nn.moments(input_tensor,[0])
-        train_mean = tf.compat.v1.assign(pop_mean, pop_mean * decay + batch_mean * (1 - decay))
-        train_var = tf.compat.v1.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
-
-        with tf.control_dependencies([train_mean, train_var]):
-            return tf.nn.batch_normalization(input_tensor, batch_mean, batch_var, beta, scale, epsilon)
-    else:
-        return tf.nn.batch_normalization(input_tensor, pop_mean, pop_var, beta, scale, epsilon)
+    return tf.compat.v1.keras.layers.BatchNormalization(inputs=input_tensor,training=is_trainable)
 
 def convolution_1d(input_tensor, number_filters, kernel_sizes=3, stride_sizes=(1,1), paddings='same', activations='relu', batch_normalizations=False, dropout_rates=None, names=None):
     """[summary]
@@ -150,7 +137,7 @@ def convolution_1d(input_tensor, number_filters, kernel_sizes=3, stride_sizes=(1
     else:
         paddings = 'SAME'
 
-    weight = new_weights(shapes=[kernel_sizes, input_tensor.get_shape().as_list()[-1], number_filters], names=names)
+    weight = new_weights(shapes=[kernel_sizes, input_tensor.get_shape().as_list()[-1], number_filters], names=names, dtypes=tf.float32)
 
     layer = tf.nn.conv1d(value=input_tensor, filters=weight, stride=stride_sizes[0], padding=paddings, use_cudnn_on_gpu=True, data_format=None, name='conv_1d_'+names)
 
@@ -200,7 +187,7 @@ def convolution_2d(input_tensor, number_filters, kernel_sizes=(3,3), stride_size
     else:
         paddings = 'SAME'
 
-    weight = new_weights(shapes=[kernel_sizes[0], kernel_sizes[1], input_tensor.get_shape().as_list()[-1], number_filters], names=names)
+    weight = new_weights(shapes=[kernel_sizes[0], kernel_sizes[1], input_tensor.get_shape().as_list()[-1], number_filters], names=names, dtypes=tf.float32)
 
     layer = tf.nn.conv2d(input=input_tensor, filter=weight, strides=[stride_sizes[0], stride_sizes[1]], padding=paddings, use_cudnn_on_gpu=True,
                          data_format='NHWC', dilations=[1, 1, 1, 1], name='conv_2d_'+names)
@@ -251,7 +238,7 @@ def convolution_3d(input_tensor, number_filters, kernel_sizes=(3,3,3), stride_si
     else:
         paddings = 'SAME'
 
-    weight = new_weights(shapes=[kernel_sizes[0], kernel_sizes[1], kernel_sizes[1], input_tensor.get_shape().as_list()[-1], number_filters], names=names)
+    weight = new_weights(shapes=[kernel_sizes[0], kernel_sizes[1], kernel_sizes[1], input_tensor.get_shape().as_list()[-1], number_filters], names=names, dtypes=tf.float32)
 
     layer = tf.nn.conv3d(input,filter,strides=[stride_sizes[0],stride_sizes[1],stride_sizes],padding=paddings,data_format='NDHWC',dilations=[1, 1, 1, 1, 1],name='conv_3d_'+names)
     
@@ -302,7 +289,7 @@ def depthwise_convolution_2d(input_tensor, number_filters=1, kernel_sizes=(3,3),
     else:
         paddings = 'SAME'
 
-    weight = new_weights(shapes=[kernel_sizes[0], kernel_sizes[1], input_tensor.get_shape().as_list()[-1], number_filters], names=names)
+    weight = new_weights(shapes=[kernel_sizes[0], kernel_sizes[1], input_tensor.get_shape().as_list()[-1], number_filters], names=names, dtypes=tf.float32)
 
     layer = tf.nn.depthwise_conv2d(input=input_tensor, filter=weight, strides=[stride_sizes[0], stride_sizes[1]], padding=paddings, rate=None, name='deptwise_conv_2d_'+names, data_format=None )
 

poda/layers/merge.py
@@ -52,14 +52,7 @@ def flatten(input_tensor, names=None):
     Returns:
         [Tensor] -- [Flatten tensor]
     """
-    if names!=None:
-        names = str(names)
-    else:
-        names = 'flatten'
-        
-    # Insert name future
-    flatten_tensor = tf.contrib.layers.flatten(input_tensor)
-    return flatten_tensor
+    return tf.compat.v2.keras.layers.Flatten()(input_tensor)
 
 def maximum(input_tensor_1, input_tensor_2, names=None):
     """[summary]

poda/transfer_learning/InceptionV4.py
@@ -72,7 +72,7 @@ class InceptionV4(object):
                 max_pool_2 = max_pool_2d(input_tensor=concat_2, pool_sizes=(3,3), stride_sizes=(2,2), paddingss='VALID', names='0b_3x3')
             
             with tf.compat.v1.variable_scope('Concatenate_3'):
-            concat_3 = concatenate(list_tensor=[max_pool_2,conv_11], axis=-1, names='maxpool_3x3_conv_3x3')
+                concat_3 = concatenate(list_tensor=[max_pool_2,conv_11], axis=-1, names='maxpool_3x3_conv_3x3')
         return concat_3
     
     # Inception A

poda/transfer_learning/Vgg16_slim.py
@@ -41,7 +41,7 @@ from poda.utils import *
 slim = tf.contrib.slim
 
 def build_base_layer_model(input_tensor,num_blocks=5,scope='vgg_16'):
-    with tf.variable_scope(scope, 'vgg_16', [input_tensor]) as sc:
+    with tf.compat.v1.variable_scope(scope, 'vgg_16', [input_tensor]) as sc:
         end_points_collection = sc.original_name_scope + '_end_points'
         # Collect outputs for conv2d, fully_connected and max_pool2d.
         with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],outputs_collections=end_points_collection):
@@ -100,11 +100,11 @@ def build_top_layer_model(base_layer,num_depthwise_layer=None,
     
 def vgg16(input_tensor,num_classes=1000,num_blocks=5,num_depthwise_layer=None,num_fully_connected_layer=1,num_hidden_unit=512,activation_fully_connected=None,regularizers=None):
     net = build_base_layer_model(input_tensor=input_tensor,num_blocks=num_blocks)
-    base_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    base_var_list = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES)
     non_logit , output = build_top_layer_model(net,num_depthwise_layer=num_depthwise_layer,num_fully_connected_layer=num_fully_connected_layer,num_hidden_unit=num_hidden_unit,
                                                activation_fully_connected=activation_fully_connected,regularizers=regularizers,num_classes=num_classes)
 
-    full_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    full_var_list = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES)
 
     return non_logit , output , base_var_list , full_var_list
 